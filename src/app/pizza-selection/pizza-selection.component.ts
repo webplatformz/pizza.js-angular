9ba59df063a05cae02b3a1f3bb4a16595dd61b0b
Dominik Freier <freier.dominik@gmail.com>
2017-05-09 14:57:28 +0200

    selectable pizza and ingredients

--- src/app/pizza-selection/pizza-selection.component.ts
+++ src/app/pizza-selection/pizza-selection.component.ts
@@ -1,7 +1,6 @@
+import { Ingredient } from './../typings/ingredient';
 import { Component } from '@angular/core';
-
 import { Pizza } from "app/typings/pizza";
-import { Ingredient } from "app/typings/ingredient";
 
 @Component({
   selector: 'app-pizza-selection',
@@ -11,22 +10,55 @@ import { Ingredient } from "app/typings/ingredient";
 export class PizzaSelectionComponent {
 
   pizzas: Pizza[] = [];
-  ingredients: Ingredient[] = [];
+  ingredients: SelectableIngredient[] = [];
+
+  selectedPizza: Pizza;
 
   pizzaPrice: number = 0;
 
   constructor() { 
-    this.pizzas = [
-      {name: 'Prosciutto', price: 4.95, ingredients: ['Mozzarella', 'Tomate']},
-      {name: 'Prosciutto1', price: 4.95, ingredients: ['Mozzarella', 'Tomate']},
-      {name: 'Prosciutto2', price: 4.95, ingredients: ['Mozzarella', 'Tomate', 'Oregano', 'Pilze', 'Noch mehr Pilze']}
-    ]
+    this.pizzas = this.loadPizzas();
+    this.ingredients = this
+      .loadIngredients()
+      .map(ingredient => new SelectableIngredient(ingredient, false));
 
-    this.ingredients = [
+    this.selectedPizza = this.pizzas[0];
+    this.updatePizzaPrice();
+  }
+
+  private loadPizzas() {
+    return [
+      {name: 'Margaritha', price: 4.95, ingredients: ['Mozzarella', 'Tomate']},
+      {name: 'Prosciutto', price: 5.95, ingredients: ['Mozzarella', 'Tomate', 'Schinken']},
+      {name: 'Hawaii', price: 6.95, ingredients: ['Mozzarella', 'Tomate', 'Oregano', 'Pilze', 'Noch mehr Pilze']}
+    ];
+  }
+
+  private loadIngredients() {
+    return [
       {name: 'Schinken', price: 0.50},
       {name: 'Pilze', price: 0.50},
       {name: 'Extra Käse', price: 0.50},
       {name: 'Ananas', price: 0.50}
-    ]
+    ];
   }
+
+  onSelectPizza(pizza: Pizza) {
+    this.selectedPizza = pizza;
+    this.updatePizzaPrice();
+  }
+
+  updatePizzaPrice() {
+    this.pizzaPrice = this.selectedPizza.price + this.ingredients
+      .filter(ingredient => ingredient.selected)
+      .map(ingredient => ingredient.ingredient.price)
+      .reduce((total, price) => total + price, 0);
+  }
+}
+
+class SelectableIngredient {
+  constructor(
+    public ingredient: Ingredient,
+    public selected: boolean
+  ) {}
 }
