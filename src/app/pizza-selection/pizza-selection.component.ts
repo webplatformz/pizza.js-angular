c37962fba7624c94a83066c084f368d891128bb8
Dominik Freier <freier.dominik@gmail.com>
2017-05-09 14:29:18 +0200

    loop over ingredients and pizzas

--- src/app/pizza-selection/pizza-selection.component.ts
+++ src/app/pizza-selection/pizza-selection.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
 
+import { Pizza } from "app/typings/pizza";
+import { Ingredient } from "app/typings/ingredient";
+
 @Component({
   selector: 'app-pizza-selection',
   templateUrl: './pizza-selection.component.html',
   styleUrls: ['./pizza-selection.component.css']
 })
 export class PizzaSelectionComponent {
+
+  pizzas: Pizza[] = [];
+  ingredients: Ingredient[] = [];
+
+  pizzaPrice: number = 0;
+
+  constructor() { 
+    this.pizzas = [
+      {name: 'Prosciutto', price: 4.95, ingredients: ['Mozzarella', 'Tomate']},
+      {name: 'Prosciutto1', price: 4.95, ingredients: ['Mozzarella', 'Tomate']},
+      {name: 'Prosciutto2', price: 4.95, ingredients: ['Mozzarella', 'Tomate', 'Oregano', 'Pilze', 'Noch mehr Pilze']}
+    ]
+
+    this.ingredients = [
+      {name: 'Schinken', price: 0.50},
+      {name: 'Pilze', price: 0.50},
+      {name: 'Extra Käse', price: 0.50},
+      {name: 'Ananas', price: 0.50}
+    ]
+  }
 }
---
+++ src/app/typings/pizza.ts
@@ -0,0 +1,5 @@
+export interface Pizza {
+  name: string;
+  price: number,
+  ingredients: string[];
+}
