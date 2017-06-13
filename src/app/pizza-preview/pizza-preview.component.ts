0afff8f0bc8abac7c334298b0da80ab11406e69e
danielsuter <suter_daniel@hotmail.com>
2017-06-13 13:26:34 +0200

    Step 2: Pizza preview component.

---
+++ src/app/pizza-preview/pizza-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-preview',
+  templateUrl: './pizza-preview.component.html',
+  styleUrls: ['./pizza-preview.component.css']
+})
+export class PizzaPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
