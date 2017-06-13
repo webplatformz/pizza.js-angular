7976c60f09d781e163e52bfffc937e75b36c22e3
danielsuter <suter_daniel@hotmail.com>
2017-06-13 13:36:27 +0200

    Step 2: Extracted shopping cart.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PizzaPreviewComponent } from './pizza-preview/pizza-preview.component';
 import { PizzaSelectionComponent } from './pizza-selection/pizza-selection.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaPreviewComponent,
-    PizzaSelectionComponent
+    PizzaSelectionComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule
